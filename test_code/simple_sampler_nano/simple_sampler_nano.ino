0adb769f281e4499ca5c0fa2468d3bb45eb65160
Ithallo Guimarães <ithallojunior@outlook.com>
2020-05-31 15:28:41 -0300

    feat: added ino code to a simple but accurate enough sampler

---
+++ test_code/simple_sampler_nano/simple_sampler_nano.ino
@@ -0,0 +1,56 @@
+/*
+A simple sampler based on wait and sample. Creates
+a buffer to make it easier.
+ */
+
+#define BUFFER_SIZE 4
+#define ANALOG_PIN 3 
+#define DELTA 500//useconds, 2000Hz
+
+
+uint16_t buffer[BUFFER_SIZE];
+unsigned long int now=0, t=0;
+//unsigned long int sending_start=0, sending_delta=0;
+//unsigned long int t0=0, t1=0;
+unsigned int i = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+  //Serial.println("Starting TX");
+}
+
+
+void loop(void){  
+    
+    for(i=0;i < BUFFER_SIZE;i++){
+      //t0 = micros();
+       
+      while( (micros() - t) <= DELTA);
+      t += DELTA;
+
+      buffer[i] = analogRead(ANALOG_PIN);
+      
+      //t1 = micros() - t0; 
+    }
+  
+  // sending
+  //sending_start = micros();
+  
+  for(i=0;i < BUFFER_SIZE;i++){
+    //Serial.println( 5*(buffer[i]/1023.)); 
+
+    Serial.write(buffer[i]/256);
+    Serial.write(buffer[i]%256);   
+  
+  } 
+
+  // timing
+  //Serial.print("time(us): ");
+  //Serial.println(t1);
+  //Serial.println(sending_delta);
+
+  //sending_delta = micros() - sending_start;
+ }
+ 
+
