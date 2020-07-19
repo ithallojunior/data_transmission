139c37d345dae1e230e57d196cc1c909a8b6c60c
Ithallo Guimarães <ithallojunior@outlook.com>
2020-07-19 19:07:54 -0300

    feat: added program to clean data from NI devices

---
+++ python_code/cleaner.py
@@ -0,0 +1,49 @@
+"""
+Cleans the files that come from the acquisitions with a NI device.
+Dots must not be in the filepath, except for the extension.
+Spaces are used as delimiters.
+"""
+
+from sys import argv
+
+
+def cleaner(filepath, delimiter=' '):
+    """
+    Cleans data from an NI device to a format readable by numpy.
+    It uses spaces as delimiters.
+    """
+
+    with open(filepath, 'r') as f:
+        lines = f.readlines()[1:]  # ignoring the header
+
+    new_lines = ''
+    for line in lines:
+
+        line = line.replace(',', '.')  # data uses commas for decimals
+        line = line.replace('\r', '')
+        line = line.replace('\n', '')
+        line = line.replace('\t', delimiter)
+        line = line.replace(';', delimiter)
+
+        if 'm' in line:
+            split_line = line.split(delimiter)
+            voltage = str(float(split_line[1][:-1])/1000.)
+            line = ('%s' % delimiter).join([split_line[0], voltage])
+
+            new_lines += (line + '\n')
+
+    split_filepath = filepath.split('.')
+    new_filepath = split_filepath[0] + '_cleaned.txt'
+
+    with open(new_filepath, 'w') as f:
+        f.write(new_lines)
+
+
+if __name__ == '__main__':
+
+    try:
+        filepath = argv[1]
+    except IndexError:
+        raise Exception('You must pass a filepath as argument')
+
+    cleaner(filepath)
